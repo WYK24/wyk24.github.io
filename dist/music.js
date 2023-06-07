83665b6d40ab61a17c699fbeb51ecf1d511ee5ca
程序员小魏 <weiyikai2003@163.com>
2023-06-07 13:35:18 +0800

    add new blog

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true, //自动播放
+    audio: [
+        {
+            name: '遗失的心跳',
+            artist: '洛./1个球',
+            url: 'https://m804.music.126.net/20230607134341/8a30a21937f50d86d04fb62a7ffc866b/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/21236410031/f0ff/7470/abd2/863f2d0a78b892338994ea4ff54879fd.m4a?authSecret=00000188944af88f11a00aa460ac1b7b',
+            cover: 'https://p1.music.126.net/xSsrTys10fu6UlAjVuRBtg==/109951165683609378.jpg?param=200y200',
+        },
+    ]
+});
